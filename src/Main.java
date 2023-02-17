bcdae3e3b524847e2d895260443c57fa2a3ec15a
tokto <toktonalievaalbina72@gmail.com>
2023-02-17 13:41:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> array = new ArrayList<>();
+        List<Integer>array1 = new ArrayList<>();
+        List<Integer> array2 = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 50; i++) {
+            array.add(random.nextInt(100));
+            if(i%2==0){ array1.add(i);
+            } else array2.add(i);
+        }
+        System.out.println(array);
+        System.out.println(array1);
+        System.out.println(array2);
+
+
+
+
+            
+        }
+
+    }
